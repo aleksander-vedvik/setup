924006e48fc9cd9c8a202d4c615dd5ccce0c9a94
aleksander-vedvik <vedvik.aleksander@gmail.com>
2023-11-24 13:23:20 +0100

    updated setup

--- setup.sh
+++ setup.sh
@@ -3,10 +3,11 @@
 mkdir -p ~/.fonts
 wget --output-document firacode_nerdfont.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.0/FiraCode.zip
 unzip firacode_nerdfont.zip -d ~/.fonts
-fc-cache -f -v
+fc-cache -f
 rm firacode_nerdfont.zip
 
 curl -sS https://starship.rs/install.sh | sh
 
 mkdir -p ~/.config && cp starship.toml ~/.config/starship.toml
 
+echo "\n\nNow change the terminal font to FiraCode Nerd Font"
