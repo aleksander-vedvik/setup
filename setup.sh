7518d101670fa2743b6d6bae010c76b05316dcb6
aleksander-vedvik <vedvik.aleksander@gmail.com>
2023-11-24 13:02:22 +0100

    added starship and bashrc

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p ~/.fonts
+wget --output-document firacode_nerdfont.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.0/FiraCode.zip
+unzip firacode_nerdfont.zip -d ~/.fonts
+fc-cache -f -v
+rm firacode_nerdfont.zip
+
+curl -sS https://starship.rs/install.sh | sh
+
+mkdir -p ~/.config && cp starship.toml ~/.config/starship.toml
+
